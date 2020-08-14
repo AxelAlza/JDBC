e2d8523490d21699d5e3ec6b6f54cbb5af50a294
AxelAlza <axelalza1998@gmail.com>
2020-08-14 08:40:48 -0300

    Cambie algunas cosas para no repetir codigo

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -2,9 +2,7 @@ package Controlador;
 
 ////Esto es normal?
 import Modelo.Articulo;
-
 import Modelo.TablaArticulo;
-import Permanencia.ConexionSql;
 import Permanencia.ConexionSql.SQLArticulo;
 import Vista.ListadoArticulos;
 import Vista.Mdi;
@@ -31,13 +29,11 @@ public class Controlador {
 
     private final Mdi principal;
     private final ListadoArticulos listadoArticulos;
-
-    private final NvoArticulo nvoArt;
-
+    private final NvoArticulo nuevoArticulo;
     public Controlador(Mdi principal, ListadoArticulos listadoArticulos, NvoArticulo nvoArt) {
         this.principal = principal;
         this.listadoArticulos = listadoArticulos;
-        this.nvoArt = nvoArt;
+        this.nuevoArticulo = nvoArt;
         principal.MantenimientoArticulosBtn.addActionListener((var evt) -> {
             MantenimientoArticulosBtn(evt);
         });
@@ -50,16 +46,16 @@ public class Controlador {
         listadoArticulos.NuevoBtn.addActionListener((var evt) -> {
             NuevoBtn(evt);
         });
-        this.nvoArt.ConfirmarBtn.addActionListener((var evt) -> {
+        this.nuevoArticulo.ConfirmarBtn.addActionListener((var evt) -> {
             AgregarArticuloBaseBtn(evt);
         });
-        this.nvoArt.CancelarBtn.addActionListener((var evt) -> {
+        this.nuevoArticulo.CancelarBtn.addActionListener((var evt) -> {
             CancelarNuevoArtBtn(evt);
         });
         this.listadoArticulos.ModificarBtn.addActionListener((var evt) -> {
             ModificarBtn(evt);
         });
-        this.nvoArt.foto.addMouseListener(new MouseAdapter() {
+        this.nuevoArticulo.foto.addMouseListener(new MouseAdapter() {
 
             //Solo quiero un metodo del mouse listener que es el mouse clicked, porque tengo que sobrescribir los demas!?
             //Hay alguna forma de no tener que hacer esto?
@@ -115,11 +111,10 @@ public class Controlador {
     //ListadoArticulos.java////////
     ///////////////////////////////
     private void EliminarBtn(ActionEvent evt) {
-        if (listadoArticulos.TablaArticulos.getSelectedRow() != -1) {
-            int i = listadoArticulos.TablaArticulos.getSelectedRow();
+        int i = listadoArticulos.TablaArticulos.getSelectedRow();
+        if (i != -1) {
             Articulo A = ((TablaArticulo) listadoArticulos.TablaArticulos.getModel()).getArticulo(i);
             SQLArticulo.EliminarArticulo(A);
-
             ((TablaArticulo) listadoArticulos.TablaArticulos.getModel()).DelArticulo(i);
         } else {
             JOptionPane.showMessageDialog(listadoArticulos, "No se selecciono ningun articulo", "Error", JOptionPane.ERROR_MESSAGE);
@@ -138,54 +133,51 @@ public class Controlador {
     private void NuevoBtn(ActionEvent evt) {
 
         try {
-            nvoArt.setClosed(false);
+            nuevoArticulo.setClosed(false);
         } catch (PropertyVetoException ex) {
             Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-        nvoArt.setSize(800, 600);
-        nvoArt.setMode("I");
-        nvoArt.foto.setIcon(new ImageIcon(NvoArticulo.class.getClassLoader().getResource("ico.png")));
+        nuevoArticulo.setSize(800, 600);
+        nuevoArticulo.setMode("I");
+        nuevoArticulo.foto.setIcon(new ImageIcon(NvoArticulo.class.getClassLoader().getResource("ico.png")));
         String s = "";
-        nvoArt.Titulo.setText("Nuevo Articulo");
-        nvoArt.id_articulo.setText(s);
-        nvoArt.codigo.setText(s);
-        nvoArt.descripcion.setText(s);
-        nvoArt.fecha_fabricacion.setText(s);
-        nvoArt.precio.setText(s);
-        nvoArt.foto.setText(s);
-        nvoArt.id_articulo.setEditable(true);
-        principal.PanelPrincipal.add(nvoArt);
-        nvoArt.setVisible(true);
+        nuevoArticulo.Titulo.setText("Nuevo Articulo");
+        nuevoArticulo.id_articulo.setText(s);
+        nuevoArticulo.codigo.setText(s);
+        nuevoArticulo.descripcion.setText(s);
+        nuevoArticulo.fecha_fabricacion.setText(s);
+        nuevoArticulo.precio.setText(s);
+        nuevoArticulo.setFotofile(null);
+        nuevoArticulo.id_articulo.setEditable(true);
+        principal.PanelPrincipal.add(nuevoArticulo);
+        nuevoArticulo.setVisible(true);
 
     }
 
     private void ModificarBtn(ActionEvent evt) {
 
         try {
-            nvoArt.setClosed(false);
+            nuevoArticulo.setClosed(false);
         } catch (PropertyVetoException ex) {
             Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
         }
-        nvoArt.setMode("M");
-
-        if (listadoArticulos.TablaArticulos.getSelectedRow() != -1) {
-            nvoArt.setSize(800, 600);
-            Articulo A = ((TablaArticulo) listadoArticulos.TablaArticulos.getModel()).getArticulo(listadoArticulos.TablaArticulos.getSelectedRow());
-            //// Si lo se eso es una chanchada
-            nvoArt.Titulo.setText("Modificar Articulo");
-            nvoArt.id_articulo.setText(String.valueOf(A.getId_articulo()));
-            nvoArt.codigo.setText(String.valueOf(A.getCodigo()));
-            nvoArt.descripcion.setText(A.getDescripcion());
-            nvoArt.fecha_fabricacion.setText(A.getFecha_fabricacion());
-            nvoArt.precio.setText(String.valueOf(A.getPrecio()));
-            nvoArt.setFotofile(A.getFoto());
-            ImageIcon img_icon = new ImageIcon(A.getFoto().toString());
-            nvoArt.id_articulo.setEditable(false);
-            principal.PanelPrincipal.add(nvoArt);
-            nvoArt.setVisible(true);
-            Icon icono = new ImageIcon(img_icon.getImage().getScaledInstance(nvoArt.foto.getWidth(), nvoArt.foto.getHeight(), Image.SCALE_DEFAULT));
-            nvoArt.foto.setIcon(icono);
+        nuevoArticulo.setMode("M");
+        int i = listadoArticulos.TablaArticulos.getSelectedRow();
+        if (i != -1) {
+            nuevoArticulo.setSize(800, 600);
+            Articulo A = ((TablaArticulo) listadoArticulos.TablaArticulos.getModel()).getArticulo(i);
+            nuevoArticulo.Titulo.setText("Modificar Articulo");
+            nuevoArticulo.id_articulo.setText(String.valueOf(A.getId_articulo()));
+            nuevoArticulo.codigo.setText(String.valueOf(A.getCodigo()));
+            nuevoArticulo.descripcion.setText(A.getDescripcion());
+            nuevoArticulo.fecha_fabricacion.setText(A.getFecha_fabricacion());
+            nuevoArticulo.precio.setText(String.valueOf(A.getPrecio()));
+            nuevoArticulo.setFotofile(A.getFoto());
+            nuevoArticulo.id_articulo.setEditable(false);
+            principal.PanelPrincipal.add(nuevoArticulo);
+            nuevoArticulo.setVisible(true);
+            nuevoArticulo.foto.setIcon(A.getIconForAbm(nuevoArticulo.foto.getWidth(), nuevoArticulo.getHeight()));
 
         } else {
 
@@ -200,7 +192,7 @@ public class Controlador {
     //////////////////////////////
     public Boolean Validar() {
         Boolean Ok = true;
-        if (nvoArt.codigo.getText().isEmpty() || nvoArt.descripcion.getText().isEmpty() || nvoArt.precio.getText().isEmpty() || nvoArt.fecha_fabricacion.getText().isEmpty() || nvoArt.id_articulo.getText().isEmpty() || nvoArt.getFotoFile() == null) {
+        if (nuevoArticulo.codigo.getText().isEmpty() || nuevoArticulo.descripcion.getText().isEmpty() || nuevoArticulo.precio.getText().isEmpty() || nuevoArticulo.fecha_fabricacion.getText().isEmpty() || nuevoArticulo.id_articulo.getText().isEmpty() || nuevoArticulo.getFotoFile() == null) {
             Ok = false;
         }
 
@@ -208,60 +200,64 @@ public class Controlador {
     }
 
     private void AgregarArticuloBaseBtn(ActionEvent evt) {
+        
+        
         if (Validar()) {
             Articulo articulo = new Articulo();
-            articulo.setFoto(nvoArt.getFotoFile());
-            articulo.setId_articulo(Integer.parseInt(nvoArt.id_articulo.getText()));
-            articulo.setCodigo(Integer.parseInt(nvoArt.codigo.getText()));
-            articulo.setDescripcion(nvoArt.descripcion.getText());
-            articulo.setPrecio(Float.parseFloat(nvoArt.precio.getText()));
-            articulo.setFecha_fabricacion(nvoArt.fecha_fabricacion.getText());
-            if (nvoArt.getMode() == "I") {
-
+            articulo.setFoto(nuevoArticulo.getFotoFile());
+            articulo.setId_articulo(Integer.parseInt(nuevoArticulo.id_articulo.getText()));
+            articulo.setCodigo(Integer.parseInt(nuevoArticulo.codigo.getText()));
+            articulo.setDescripcion(nuevoArticulo.descripcion.getText());
+            articulo.setPrecio(Float.parseFloat(nuevoArticulo.precio.getText()));
+            articulo.setFecha_fabricacion(nuevoArticulo.fecha_fabricacion.getText());
+            if (nuevoArticulo.getMode() == "I") {
+                
+                //Este metodo devuelve null si no existe un articulo con el mismo id 
                 Articulo art = SQLArticulo.CheckExistente(articulo.getId_articulo());
+                ////////////////////////////////////////////////////////////////////
+                
                 System.out.println(art);
                 if (art == null) {
                     SQLArticulo.InsertarArticulo(articulo);
                     ((TablaArticulo) listadoArticulos.TablaArticulos.getModel()).AddArticulo(articulo);
                     try {
-                        nvoArt.setClosed(true);
+                        nuevoArticulo.setClosed(true);
                     } catch (PropertyVetoException ex) {
                         Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
                     }
                 } else {
-                    JOptionPane.showMessageDialog(nvoArt, "Ya existe un articulo con ese id", "error", JOptionPane.ERROR_MESSAGE);
-                    nvoArt.setMode("M");
-                    nvoArt.Titulo.setText("Modificar Articulo");
-                    nvoArt.id_articulo.setText(String.valueOf(art.getId_articulo()));
-                    nvoArt.codigo.setText(String.valueOf(art.getCodigo()));
-                    nvoArt.descripcion.setText(art.getDescripcion());
-                    nvoArt.fecha_fabricacion.setText(art.getFecha_fabricacion());
-                    nvoArt.precio.setText(String.valueOf(art.getPrecio()));
-                    nvoArt.setFotofile(art.getFoto());
-                    ImageIcon img_icon = new ImageIcon(art.getFoto().toString());
-                    nvoArt.id_articulo.setEditable(false);
-                    Icon icono = new ImageIcon(img_icon.getImage().getScaledInstance(nvoArt.foto.getWidth(), nvoArt.foto.getHeight(), Image.SCALE_DEFAULT));
-                    nvoArt.foto.setIcon(icono);
+                    JOptionPane.showMessageDialog(nuevoArticulo, "Ya existe un articulo con ese id", "error", JOptionPane.ERROR_MESSAGE);
+                    nuevoArticulo.setMode("M");
+                    nuevoArticulo.Titulo.setText("Modificar Articulo");
+                    nuevoArticulo.id_articulo.setText(String.valueOf(art.getId_articulo()));
+                    nuevoArticulo.codigo.setText(String.valueOf(art.getCodigo()));
+                    nuevoArticulo.descripcion.setText(art.getDescripcion());
+                    nuevoArticulo.fecha_fabricacion.setText(art.getFecha_fabricacion());
+                    nuevoArticulo.precio.setText(String.valueOf(art.getPrecio()));
+                    nuevoArticulo.setFotofile(art.getFoto());                   
+                    nuevoArticulo.id_articulo.setEditable(false);            
+                    nuevoArticulo.foto.setIcon(art.getIconForAbm(nuevoArticulo.getWidth(), nuevoArticulo.getHeight()));
                 }
-            } else if (nvoArt.getMode() == "M") {
+                
+            } else if (nuevoArticulo.getMode() == "M") {
                 SQLArticulo.ModificarArticulo(articulo);
                 ((TablaArticulo) listadoArticulos.TablaArticulos.getModel()).SetArticulo(listadoArticulos.TablaArticulos.getSelectedRow(), articulo);
                 try {
-                    nvoArt.setClosed(true);
+                    nuevoArticulo.setClosed(true);
                 } catch (PropertyVetoException ex) {
                     Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
                 }
             }
 
         } else {
-            JOptionPane.showMessageDialog(nvoArt, "Hay campos vacios", "Error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(nuevoArticulo, "Hay campos vacios", "Error", JOptionPane.ERROR_MESSAGE);
         }
 
     }
 
     private void CancelarNuevoArtBtn(ActionEvent evt) {
         try {
-            nvoArt.setClosed(true);
+            nuevoArticulo.setClosed(true);
         } catch (PropertyVetoException ex) {
             Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
         }
